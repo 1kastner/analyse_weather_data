2da0e9d0a94e48af05e7900831518b45191f6cc0
Marvin Kastner <1kastner@users.noreply.github.com>
2017-10-10 18:46:13 +0200

    plot sample night.

--- plot_weather_data/plot_husconet_temperatures.py
+++ plot_weather_data/plot_husconet_temperatures.py
@@ -2,8 +2,10 @@
 Depends on filter_weather_data.filters.preparation.average_husconet_temperature
 """
 import logging
+import itertools
 
 from matplotlib import pyplot
+from matplotlib import dates as mdates
 
 from gather_weather_data.husconet import HUSCONET_STATIONS
 from gather_weather_data.husconet import OFFICIAL_HUSCONET_NAME
@@ -20,20 +22,34 @@ def plot_stations():
     fig.canvas.set_window_title("husconet year 2016")
     pyplot.rcParams['savefig.dpi'] = 300
 
+    temperatures = []
     for station_name in HUSCONET_STATIONS:
-        station_df = load_husconet_station(station_name, "2016-01-01", "2016-12-31", "temperature")
+        station_df = load_husconet_station(station_name, "2016-05-04T00:00", "2016-05-05T12:01", "temperature")
         logging.debug("plotting {station} from {start} to {end}"
                       .format(station=station_name, start=station_df.index.min(), end=station_df.index.max()))
         official_station_name = OFFICIAL_HUSCONET_NAME[station_name]
-        pyplot.plot(station_df.index, station_df.temperature, label=official_station_name, alpha=.3, linewidth=.4)
+        pyplot.plot(station_df.index, station_df.temperature, label=official_station_name, linewidth=1)
+        temperatures.append(station_df.temperature)
 
     logging.debug("start plotting")
 
-    style_year_2016_plot(pyplot.gca())
+    ax = pyplot.gca()
+    style_year_2016_plot(ax)
+    ax.xaxis.set_major_locator(mdates.HourLocator(byhour=[0, 6, 12, 18]))
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%d.%m %H:%M"))
+    fig.autofmt_xdate(bottom=0.2, rotation=50, ha='right')
+    ax.set_xlabel('')
     leg = pyplot.legend()
     for line in leg.get_lines():
         line.set_linewidth(1)  # .4 is too small
-    pyplot.show()
+    #pyplot.show()
+
+    max_temperature_diff = 0
+    for t1, t2 in itertools.combinations(temperatures, 2):
+        _max_temperature_diff = (t1 - t2).max()
+        if _max_temperature_diff > max_temperature_diff:
+            max_temperature_diff = _max_temperature_diff
+    print("max temperature diff", max_temperature_diff)
 
 
 if __name__ == "__main__":
